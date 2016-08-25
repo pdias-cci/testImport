5b689b6a6e796d03b7527efb555d8c9f039c7436
Presley dias <presley.dias@creativecapsule.com>
2016-08-25 15:39:32 +0530

    Paginated code update

--- backend/app/controllers/user.js
+++ backend/app/controllers/user.js
@@ -16,22 +16,25 @@ var User = function() {
 
     //Echo message
     this.echoHello = function(req, res) {
-        console.log("I m here");
+        console.log("I m here echoHello");
         return res.status(200).json({ echo: "Hello world" });
     };
 
     //...Get all users
     this.getUsersList = function(req, res) {
-        console.log("I m here");
+        console.log("I m here getUsersList");
         return res.status(200).json({ users: "Hello" });
     };
 
     //GET GITHUB user by pagination
+    //CALL this http://localhost:7888/user/UsersListByRange?value=2
     this.UsersListByRange = function(req, res) {
-        console.log("I am in UsersListByRange");
+
+        var SinceValue = req.query.value;
+        console.log("I am in UsersListByRange : " + SinceValue);
 
         var options = {
-            url: 'https://api.github.com/users?since=135',
+            url: 'https://api.github.com/users?since='+SinceValue,
             headers: {
                 'User-Agent': 'request'
             }
@@ -40,8 +43,7 @@ var User = function() {
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
                 var info = JSON.parse(body);
-                console.log("info : " + JSON.stringify(body));
-                
+                //console.log("info : " + JSON.stringify(body));
                 return res.status(200).json({ users: info });
             }
         }
