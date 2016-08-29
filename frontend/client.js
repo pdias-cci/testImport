313e7a0c3809ce03355b7c8c7e32c8d391a7069b
Presley dias <presley.dias@creativecapsule.com>
2016-08-29 15:55:50 +0530

    Node_module removed

---
+++ frontend/client.js
@@ -0,0 +1,15 @@
+// Required Modules
+var express    = require("express");
+var app        = express();
+var port = process.env.PORT || 9888;
+
+app.use(express.static("./client"));
+
+app.get("/", function(req, res) {
+    res.sendFile("./client/index.html");
+});
+
+// Start Server
+app.listen(port, function () {
+    console.log( "Express server listening on port " + port);
+});
