6e27ca6b6c512435232ad8f6f76b59b72473bdb4
Presley dias <presley.dias@creativecapsule.com>
2016-09-01 10:22:34 +0530

    New js file for constants added.
    This fill will have all the constants for the Application. Including
    messages.

---
+++ frontend/client/constants/constants.app.js
@@ -0,0 +1,11 @@
+/*
+	Description : All App contants will be added here. Example Port, formats and messages.
+    Author : PresleyDias
+*/
+
+
+var constants = {
+    constAppName: 'Gitlib',
+    constFullURL: 'http://localhost' + ':' + '3000'
+};
+angular.module('Gitlib').constant('constants', constants);
